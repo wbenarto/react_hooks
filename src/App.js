df63c0df6fded376289e5258596e200b3ebb3424
w.benarto <w.benarto@gmail.com>
2019-08-08 22:00:14 -0700

    done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import './App.css'
 
 // Todo component, bring in 3 props using destructuring
-function Todo({ todo, index }) {
+function Todo({ todo, index, completeTodo }) {
   return (
-    <div className='todo'>
+    <div style={{textDecoration: todo.isCompleted ? 'line-through' : ''}} className='todo'>
       {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+      </div>
     </div>
   )
 }
@@ -46,19 +49,25 @@ function App() {
       text: 'Build really cool todo app',
       isCompleted: false
     }
-  ])
+  ]);
 
   const addTodo = text => {
     const newTodos = [...todos, { text }];
     setTodos(newTodos);
-  }
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
 
   return (
     <div className='app'>
       <div className='todo-list'>
         {/* map through the state === for each to do,, do something*/}
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo key={index} index={index} todo={todo} completeTodo={completeTodo} />
         ))}
         <TodoForm addTodo={addTodo} />
       </div>
